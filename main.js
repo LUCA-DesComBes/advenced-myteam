b20b1f30f1aee2699d5db81d92bddffddda7772f
LUCA-DesComBes <ldescombes@edenschool.fr>
2024-03-05 14:21:44 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function openMenuMobile() {
+    document.querySelector('.header__nav__div').classList.add('open')
+    document.querySelector('.header__nav__opacity').classList.add('open')
+}
+function closeMenuMobile() {
+    document.querySelector('.header__nav__div').classList.remove('open')
+    document.querySelector('.header__nav__opacity').classList.remove('open')
+}
